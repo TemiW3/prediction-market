82ce6ff01de71909c4b050bef2786951596e51e0
Temi <temiw3@gmail.com>
2026-01-18 21:24:02 +0000

    added some tightening up to be safe

--- programs/prediction-market/src/instructions/update_feed.rs
+++ programs/prediction-market/src/instructions/update_feed.rs
@@ -1,10 +1,13 @@
 use anchor_lang::prelude::*;
 use crate::state::*;
 use crate::errors::*;
+use switchboard_solana::ID as SWITCHBOARD_PROGRAM_ID;
 
 pub fn update_oracle_feed(
     ctx: Context<UpdateOracleFeed>
 ) -> Result<()> {
+    let clock = Clock::get()?;
+    require!(clock.unix_timestamp < ctx.accounts.market.start_time, PredictionMarketError::MarketAlreadyStarted);
     require!(
         ctx.accounts.authority.key() == ctx.accounts.market.authority,
         PredictionMarketError::UnauthorizedUpdater
@@ -21,7 +24,8 @@ pub struct UpdateOracleFeed<'info> {
     #[account(mut)]
     pub market: Account<'info, Market>,
     
-    /// CHECK: New Oracle feed
+    /// CHECK: New Switchboard oracle feed (owner check)
+    #[account(owner = SWITCHBOARD_PROGRAM_ID)]
     pub oracle_feed: AccountInfo<'info>,
     
     pub authority: Signer<'info>,
